5db56737c8167fc67ea70f0f62f920a2355d4406
anas12312 <anashesham000@gmail.com>
2025-02-18 14:56:02 +0200

    Fix: cors issue

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import WorkerRouter from "./api/WorkerRouter";
 import jobsRouter from "./api/JobsRouter";
 import { auth } from "./middlewares/Auth";
 import { initializeSocket } from './socket.client';
+import cors from 'cors';
 console.log('ENV:' + process.env.NODE_ENV);
 
 const app = express();
@@ -16,7 +17,9 @@ const socket = initializeSocket();
 
 // JSON Parser Middleware
 app.use(json());
-
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+}));
 // Routers Middleware
 
 app.use('/user', UserRouter);
